9def4e344ade2985f782940c8cf77f34764ccae7
luz <luzma_87@yahoo.com>
2017-10-23 20:40:52 -0500

    limpia el codigo

---
+++ app/src/main/java/uio/androidbootcamp/fragmentstabs/SectionsPagerAdapter.java
@@ -0,0 +1,35 @@
+package uio.androidbootcamp.fragmentstabs;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "SECTION 1";
+            case 1:
+                return "SECTION 2";
+        }
+        return null;
+    }
+}
