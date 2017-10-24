e391f71c9dd37262e2f6185e24d2293e7338dcc2
luz <luzma_87@yahoo.com>
2017-10-23 20:50:40 -0500

    crea Pokemon fragment con una pequenia lista de pokemon

---
+++ app/src/main/java/uio/androidbootcamp/fragmentstabs/PokemonFragment.java
@@ -0,0 +1,28 @@
+package uio.androidbootcamp.fragmentstabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PokemonFragment extends Fragment {
+
+    public PokemonFragment() {
+        // Required empty public constructor
+    }
+
+    public static PokemonFragment newInstance() {
+        return new PokemonFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pokemon, container, false);
+    }
+}
